1d7155cd52c07f26f487e42bbd16d3a3bafe4ea1
pedroobando <pedroobando@hotmail.com>
2021-07-09 05:37:27 -0400

    ajuste index

--- src/index.js
+++ src/index.js
@@ -22,7 +22,6 @@ const server = new ApolloServer({
     if (token) {
       try {
         const usuario = jwt.verify(token.replace('Bearer ', ''), process.env.SECRETA);
-        // console.log(usuario);
         return { usuario };
       } catch (error) {
         console.log('Error en Token');
